22eb183ac7994ec26d075316e8ae07d77e67276e
Raha <rahanaseri1365@yahoo.com>
2016-04-19 11:22:04 +0200

    Added files via upload

---
+++ login.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<br></br>
+<b size="4" color="black">You Can not watch this video unless you are logged in 
+to your wordpress account!</b>
+<br></br>
+<br></br>
+
+//<a href="<?php echo wp_login_url(); ?>" title="Login"><font size="4" color="FF00CC">Login</font></a>
+
+</body>
+</html>
